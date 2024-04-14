f973a53585a52cc78cb66c3c99056d9968624ac3
mhdi-742 <149945447+mhdi-742@users.noreply.github.com>
2024-04-15 02:45:41 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,19 +10,11 @@ let cors=require('cors');
 mongoose.connect(url)
 .then((res)=>console.log("connected"))
 .catch((err)=>console.log(err));
-app.use(cors())
+app.use(cors());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+  });
 app.listen("https://movieapp-api-742.vercel.app");
 app.use(express.json());
-app.use((req, res, next) => {
-      res.setHeader("Access-Control-Allow-Origin", "*");
-      res.setHeader(
-        "Access-Control-Allow-Methods",
-        "OPTIONS, GET, POST, PUT, PATCH, DELETE"
-      );
-      res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-      if (req.method === "OPTIONS") {
-        return res.sendStatus(200);
-      }
-      next();
-    });
 app.use(routes);
