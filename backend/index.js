82cc51b581b54cb0f7946fc809010d2cd3a65ce5
mhdi-742 <149945447+mhdi-742@users.noreply.github.com>
2024-05-30 00:41:39 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const bcrypt=require('bcryptjs')
 const app=express();
 const User=require('./models/User')
 const mongoose=require('mongoose');
-const url="mongodb+srv://mh7427778:Mh%407427778@blog.bakmqvn.mongodb.net/";
+
 let cors=require('cors');
 mongoose.connect(url)
 .then((res)=>console.log("connected"))
